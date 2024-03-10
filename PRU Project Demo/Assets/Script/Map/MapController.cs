165a30e2212f915d5ceb7c4613302083a089e42f
Hà Việt Danh <havietdanh1810@gmail.com>
2024-03-10 08:48:25 +0700

    sung

--- PRU Project Demo/Assets/Script/Map/MapController.cs
+++ PRU Project Demo/Assets/Script/Map/MapController.cs
@@ -33,74 +33,74 @@ public class MapController : MonoBehaviour
             return;
         }
 
-        if (pm.moveDir.x > 0 && pm.moveDir.y == 0) //Right
-        {
+        //if (pm.moveDir.x > 0 && pm.moveDir.y == 0) //Right
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Right").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Right").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x < 0 && pm.moveDir.y == 0) //Left
-        {
+        //}
+        //else if (pm.moveDir.x < 0 && pm.moveDir.y == 0) //Left
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Left").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Left").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x == 0 && pm.moveDir.y > 0) //Up
-        {
+        //}
+        //else if (pm.moveDir.x == 0 && pm.moveDir.y > 0) //Up
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Up").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Up").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x == 0 && pm.moveDir.y < 0) //Down
-        {
+        //}
+        //else if (pm.moveDir.x == 0 && pm.moveDir.y < 0) //Down
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Down").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Down").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x > 0 && pm.moveDir.y > 0) //Right Up
-        {
+        //}
+        //else if (pm.moveDir.x > 0 && pm.moveDir.y > 0) //Right Up
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Right Up").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Right Up").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x > 0 && pm.moveDir.y < 0) //Right Down
-        {
+        //}
+        //else if (pm.moveDir.x > 0 && pm.moveDir.y < 0) //Right Down
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Right Down").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Right Down").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x < 0 && pm.moveDir.y > 0) //Left Up
-        {
+        //}
+        //else if (pm.moveDir.x < 0 && pm.moveDir.y > 0) //Left Up
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Left Up").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Left Up").position;
                 SpawnTerrain();
             }
-        }
-        else if (pm.moveDir.x < 0 && pm.moveDir.y < 0) //Left Down
-        {
+        //}
+        //else if (pm.moveDir.x < 0 && pm.moveDir.y < 0) //Left Down
+        //{
             if (!Physics2D.OverlapCircle(currentTerrain.transform.Find("Left Down").position, radius, terrainMask))
             {
                 terrainPosition = currentTerrain.transform.Find("Left Down").position;
                 SpawnTerrain();
             }
-        }
+        //}
     }
 
     void SpawnTerrain()
     {
-        Instantiate(terrains[0], terrainPosition, Quaternion.identity);
+        Instantiate(terrains[0], terrainPosition, Quaternion.identity, GameObject.Find("Map").transform);
     }
 }
